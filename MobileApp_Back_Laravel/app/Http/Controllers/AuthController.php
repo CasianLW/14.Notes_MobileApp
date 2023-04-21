d8a2cbce924d059061e70176d8c897563714d173
CasianLW <ciorbacasian@gmail.com>
2023-04-21 10:24:27 +0200

    Methodes register / login Validators, token generation & db manipulation

---
+++ MobileApp_Back_Laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
+    }
+
+    public function login(Request $request)
+{
+    $credentials = $request->only('email', 'password');
+
+    if (!Auth::attempt($credentials)) {
+        return response()->json(['error' => 'Email ou mot de passe incorrects'], 401);
+    }
+
+    // Retrieve the authenticated user instance from the User model
+    $user = User::find(Auth::id());
+    
+    // Generate the authentication token
+    $token = $user->createToken('authToken')->plainTextToken;
+
+    return response()->json(['user' => $user, 'token' => $token]);
+}
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Deconnexion réussie !']);
+    }
+}
