36c4d8de000109a40965244e015e15fc5a7e5dca
CasianLW <ciorbcasian@gmail.com>
2023-05-21 22:13:09 +0200

    new router with app pages

--- MobileApp_Front_IonicVue/src/router/index.ts
+++ MobileApp_Front_IonicVue/src/router/index.ts
@@ -1,39 +1,38 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+// import TabsPage from "../views/TabsPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/tabs/tab1'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
-]
+    path: "/home",
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    path: "/login",
+    component: () => import("@/views/Login.vue"),
+  },
+  {
+    path: "/register",
+    component: () => import("@/views/Register.vue"),
+  },
+  {
+    path: "/notes",
+    component: () => import("@/views/Notes.vue"),
+  },
+  {
+    path: "/notes/:id",
+    component: () => import("@/views/NoteDetail.vue"),
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
