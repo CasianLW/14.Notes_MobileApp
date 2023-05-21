8f38325e3101f32cbd49e8babfd34c8c4d2d38f1
CasianLW <ciorbcasian@gmail.com>
2023-05-21 03:32:12 +0200

    updated reset controller

--- MobileApp_Back_Laravel/app/Http/Controllers/ResetController.php
+++ MobileApp_Back_Laravel/app/Http/Controllers/ResetController.php
@@ -10,9 +10,11 @@ class ResetController extends Controller
 {
     public function reset()
     {
-        User::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Note::truncate();
+        User::truncate();
         DB::table('personal_access_tokens')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         return response(null, 204);
     }
