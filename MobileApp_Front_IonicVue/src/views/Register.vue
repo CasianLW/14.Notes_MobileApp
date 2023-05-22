1e9e9b6e009573fc7a11f6cfa3fd61905583aa25
CasianLW <ciorbacasian@gmail.com>
2023-05-23 01:23:25 +0200

    error cases & form validation & design

--- MobileApp_Front_IonicVue/src/views/Register.vue
+++ MobileApp_Front_IonicVue/src/views/Register.vue
@@ -12,72 +12,71 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Register page" />
       <main>
-        <h1>Register</h1>
-
-        <Error :error="hasError" :errors="errorMessages" />
+        <!-- <Error :error="hasError" :errors="errorMessages" /> -->
 
         <form @submit.prevent="register">
-          <table border="0">
-            <tbody>
-              <tr>
-                <td>Username:</td>
-                <td>
-                  <input
-                    type="text"
-                    size="20"
-                    v-model="values.username"
-                    autocomplete="username"
-                  />
-                  <span v-if="errors.username" class="error">{{
-                    errors.username[0]
-                  }}</span>
-                </td>
-              </tr>
-              <tr>
-                <td>Email:</td>
-                <td>
-                  <input
-                    type="email"
-                    size="20"
-                    v-model="values.email"
-                    autocomplete="email"
-                  />
-                  <span v-if="errors.email" class="error">{{
-                    errors.email[0]
-                  }}</span>
-                </td>
-              </tr>
-              <tr>
-                <td>Password:</td>
-                <td>
-                  <input
-                    type="password"
-                    size="20"
-                    v-model="values.password"
-                    autocomplete="current-password"
-                  />
-                  <span v-if="errors.password" class="error">{{
-                    errors.password[0]
-                  }}</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <button type="submit">Register</button>
+          <ion-item>
+            <ion-label position="stacked">Username:</ion-label>
+            <Field name="username" :rules="usernameRules">
+              <template #default="{ field }">
+                <ion-input
+                  type="text"
+                  v-bind="field"
+                  autocomplete="username"
+                ></ion-input>
+                <div v-if="field.value">
+                  <ErrorMessage name="username" />
+                </div>
+              </template>
+            </Field>
+          </ion-item>
+          <ion-item>
+            <ion-label position="stacked">Email:</ion-label>
+            <Field name="email" :rules="emailRules">
+              <template #default="{ field }">
+                <ion-input
+                  type="email"
+                  v-bind="field"
+                  autocomplete="email"
+                ></ion-input>
+                <div v-if="field.value">
+                  <ErrorMessage name="email" />
+                </div>
+              </template>
+            </Field>
+          </ion-item>
+          <ion-item>
+            <ion-label position="stacked">Password:</ion-label>
+            <Field name="password" :rules="passwordRules">
+              <template #default="{ field }">
+                <ion-input
+                  type="password"
+                  v-bind="field"
+                  autocomplete="current-password"
+                ></ion-input>
+                <div v-if="field.value">
+                  <ErrorMessage name="password" />
+                </div>
+              </template>
+            </Field>
+          </ion-item>
+
+          <ion-button expand="full" type="submit">Register</ion-button>
         </form>
+        <div class="ion-text-center" v-if="activeSpinner">
+          <br />
+          <br />
+          <ion-spinner color="primary"></ion-spinner>
+        </div>
+        <div v-if="error" class="error ion-text-center">{{ errorMessage }}</div>
       </main>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { ref, reactive } from "vue";
-import { useField, useForm } from "vee-validate";
-// import { required, email, min } from "@vee-validate/rules";
-import Error from "../components/Error.vue";
+<script lang="ts">
+import { ref } from "vue";
 import { useAuthStore } from "../stores/auth";
 import { useRouter } from "vue-router";
 import {
@@ -86,61 +85,121 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonSpinner,
 } from "@ionic/vue";
 import ExploreContainer from "@/components/ExploreContainer.vue";
-import { email, minLength, required } from "@vuelidate/validators";
+import {
+  useForm,
+  Field,
+  ErrorMessage,
+  configure,
+  defineRule,
+} from "vee-validate";
+import { required, min, email } from "@vee-validate/rules";
 
-interface Form {
-  username: string;
-  email: string;
-  password: string;
-}
-
-interface ErrorObject {
-  $property: string;
-  $message: string;
-}
-
-const form: Form = reactive({
-  username: "",
-  email: "",
-  password: "",
+defineRule("customRequired", (value: string) => {
+  if (!value || value.trim() === "") {
+    return "Champ obligatoire";
+  }
+  return true;
 });
 
-// const validationSchema = {
-//   username: {},
-//   email: {
-//     email,
-//   },
-//   password: {
-//     min: minLength(8),
-//   },
-// };
+defineRule("required", required);
+defineRule("email", email);
+defineRule("customEmail", (value: string) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-const { handleSubmit, resetForm, values, errors } = useForm<Form>({});
-
-const hasError = ref(false);
-const errorMessages = ref<ErrorObject[]>([]);
-
-const authStore = useAuthStore();
-const router = useRouter();
-
-async function register() {
-  console.log(values.username);
-  try {
-    console.log(values.username);
-    await authStore.register(values.username, values.email, values.password);
-    router.push({ path: "/home" });
-  } catch (error) {
-    if (error instanceof Error) {
-      hasError.value = true;
-      errorMessages.value.push({
-        $property: error.name,
-        $message: error.message,
-      });
-    }
+  if (!value || !emailRegex.test(value)) {
+    return "Entrez une adresse email valide";
   }
-}
+
+  return true;
+});
+defineRule("min", min);
+defineRule("password", (value: string) => {
+  if (!/(?=.*[A-Z])/.test(value)) {
+    return "Le mot de passe doit contenir au moins une lettre capitale";
+  }
+
+  return true;
+});
+
+configure({
+  generateMessage: (ctx) => {
+    const messages = {
+      customRequired: "Champ obligatoire",
+      customEmail: "Entrez une adresse email valide",
+      password: "Le mot de passe doit contenir au moins une lettre capitale",
+      required: "Ce champ est obligatoire",
+      min: "Le mot de passe doit avoir au moins 8 caractères",
+      email: "Entrez une adresse email valide",
+    };
+
+    const messageKey = ctx.rule.name as keyof typeof messages;
+
+    return messages[messageKey] ?? "This field is invalid";
+  },
+});
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainer,
+    Field,
+    ErrorMessage,
+    IonSpinner,
+  },
+  setup() {
+    const { handleSubmit, resetForm, errors } = useForm();
+    const activeSpinner = ref(false);
+
+    const error = ref(false);
+    const errorMessage = ref("");
+
+    const usernameRules = "required";
+    const emailRules = "customRequired|email";
+    const passwordRules = "required|min:8|password";
+
+    const authStore = useAuthStore();
+    const router = useRouter();
+
+    const register = handleSubmit(async (values) => {
+      activeSpinner.value = true;
+      try {
+        await authStore.register(
+          values.username,
+          values.email,
+          values.password
+        );
+        router.push({ path: "/home" });
+      } catch (err) {
+        error.value = true;
+        console.log(err);
+        errorMessage.value = "Register failed, try again";
+      }
+      activeSpinner.value = false;
+      setTimeout(() => {
+        errorMessage.value = "";
+      }, 3000);
+    });
+
+    return {
+      handleSubmit,
+      errors,
+      error,
+      errorMessage,
+      usernameRules,
+      emailRules,
+      passwordRules,
+      register,
+      activeSpinner,
+    };
+  },
+};
 </script>
 
 <style scoped>
