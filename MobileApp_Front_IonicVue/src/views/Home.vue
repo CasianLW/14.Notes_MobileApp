bddf08ec2fa319c207a817c0e2a940d9b39a6166
CasianLW <ciorbcasian@gmail.com>
2023-05-22 02:12:50 +0200

    homepage variable en fonction de si connecté ou pas

--- MobileApp_Front_IonicVue/src/views/Home.vue
+++ MobileApp_Front_IonicVue/src/views/Home.vue
@@ -2,22 +2,66 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Homepage</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Homepage</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <ExploreContainer name="Tab 1 page" />
+
+      <div v-if="loggedIn">
+        <h1>Hello {{ user.user.name }}</h1>
+        Voici votre dashboard : <br />
+        vide.
+      </div>
+
+      <div v-else>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/register">Register</router-link>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+import { ref } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
+import { useAuthStore } from "@/stores/auth";
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainer,
+  },
+  setup() {
+    const authStore = useAuthStore();
+    const loggedIn = ref(authStore.loggedIn);
+    const user = ref(authStore.user);
+
+    return {
+      loggedIn,
+      user,
+    };
+  },
+};
 </script>
+
+<style scoped>
+/* Your component styles here */
+</style>
