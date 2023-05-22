c7fded3d052776ecd3705ddc0a601145ac2e4563
CasianLW <ciorbacasian@gmail.com>
2023-05-22 19:21:20 +0200

    Notes logic (list exisitng notes + create note form /methds)

--- MobileApp_Front_IonicVue/src/views/Notes.vue
+++ MobileApp_Front_IonicVue/src/views/Notes.vue
@@ -1,28 +1,77 @@
 <template>
   <ion-page>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Notes</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-item>
+        <ion-label position="floating">New Note</ion-label>
+        <ion-textarea v-model="newNote.content"></ion-textarea>
+        <ion-button @click="createNote">Add Note</ion-button>
+      </ion-item>
+      <ion-list>
+        <ion-item v-for="note in notes" :key="note.id">
+          <p>{{ note.content }}</p>
+          <ion-button @click="deleteNote(note.id)">Delete</ion-button>
+          <ion-button @click="updateNote(note.id, note)">Edit</ion-button>
+        </ion-item>
+      </ion-list>
+    </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonButton,
+  IonLabel,
+  IonTextarea,
+} from "@ionic/vue";
+import { ref, onMounted } from "vue";
+import { useNotesStore } from "@/stores/notes";
+
+const notesStore = useNotesStore();
+const notes = ref([]);
+const newNote = ref({ content: "" });
+
+onMounted(async () => {
+  await notesStore.fetchNotes();
+  notes.value = notesStore.notes;
+});
+
+const createNote = async () => {
+  if (newNote.value.content !== "") {
+    await notesStore.createNote(newNote.value);
+    newNote.value = { content: "" };
+    // re-fetch notes to include the new one
+    await notesStore.fetchNotes();
+    notes.value = notesStore.notes;
+  }
+};
+
+const deleteNote = async (id) => {
+  await notesStore.deleteNote(id);
+  // re-fetch notes to exclude the deleted one
+  await notesStore.fetchNotes();
+  notes.value = notesStore.notes;
+};
+
+const updateNote = async (id, updatedNote) => {
+  await notesStore.updateNote(id, updatedNote);
+  // re-fetch notes to include the updated note
+  await notesStore.fetchNotes();
+  notes.value = notesStore.notes;
+};
 </script>
+
+<!-- <style scoped>
+
+</style> -->
