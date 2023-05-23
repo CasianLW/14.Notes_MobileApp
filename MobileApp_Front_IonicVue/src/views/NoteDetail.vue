eaf48cb953cd7279cd4a8e692e12801f14392635
CasianLW <ciorbcasian@gmail.com>
2023-05-23 02:23:17 +0200

    design update & onmount imported get note infos & spinner logic

--- MobileApp_Front_IonicVue/src/views/NoteDetail.vue
+++ MobileApp_Front_IonicVue/src/views/NoteDetail.vue
@@ -2,16 +2,37 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/notes"></ion-back-button>
+        </ion-buttons>
         <ion-title>Edit Note</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-item>
-        <ion-label position="floating">Edit Note</ion-label>
-        <ion-textarea v-model="note.content"></ion-textarea>
-        <ion-button @click="updateNote">Update Note</ion-button>
-      </ion-item>
-      <ion-button @click="goBack">Back to Notes</ion-button>
+    <ion-content class="ion-padding">
+      <ion-card class="edit-note-card">
+        <ion-card-content>
+          <ion-item>
+            <ion-label position="floating">Edit Note</ion-label>
+            <ion-textarea
+              v-model="note.content"
+              :placeholder="note.content"
+            ></ion-textarea>
+          </ion-item>
+          <ion-button expand="full" @click="updateNote">Update Note</ion-button>
+          <ion-button
+            fill="outline"
+            color="danger"
+            expand="full"
+            @click="deleteNote"
+            >Delete Note</ion-button
+          >
+        </ion-card-content>
+      </ion-card>
+      <div class="ion-text-center" v-if="activeSpinner">
+        <br />
+        <br />
+        <ion-spinner color="primary"></ion-spinner>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -21,12 +42,17 @@ import {
   IonPage,
   IonHeader,
   IonToolbar,
+  IonButtons,
+  IonBackButton,
   IonTitle,
   IonContent,
+  IonCard,
+  IonCardContent,
   IonItem,
   IonLabel,
   IonTextarea,
   IonButton,
+  IonSpinner,
 } from "@ionic/vue";
 import { ref, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
@@ -36,26 +62,34 @@ const notesStore = useNotesStore();
 const router = useRouter();
 const route = useRoute();
 const note = ref({ id: route.params.id, content: "" });
+const activeSpinner = ref(false);
 
 onMounted(async () => {
-  const noteDetails = notesStore.notes.find(
-    (note) => note.id === route.params.id
-  );
-  if (noteDetails) {
-    note.value.content = noteDetails.content;
-  } else {
-    await notesStore.fetchNotes();
-    const fetchedNote = notesStore.notes.find(
-      (note) => note.id === route.params.id
-    );
-    if (fetchedNote) {
-      note.value.content = fetchedNote.content;
-    }
+  activeSpinner.value = true;
+
+  const fetchedNote = await notesStore.fetchNoteById(route.params.id);
+  if (fetchedNote) {
+    note.value.content = fetchedNote.content;
   }
+  activeSpinner.value = false;
 });
 
 const updateNote = async () => {
+  activeSpinner.value = true;
+
   await notesStore.updateNote(note.value.id, note.value);
+  activeSpinner.value = false;
+
+  // router.push("/notes");
+};
+
+const deleteNote = async () => {
+  activeSpinner.value = true;
+
+  await notesStore.deleteNote(note.value.id);
+  await notesStore.fetchNotes();
+  activeSpinner.value = false;
+
   router.push("/notes");
 };
 
@@ -63,3 +97,9 @@ const goBack = () => {
   router.push("/notes");
 };
 </script>
+
+<style scoped>
+.edit-note-card {
+  margin-bottom: 20px;
+}
+</style>
